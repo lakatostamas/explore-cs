b5055366589a0055eddc6ee86f6c2d48e2de2601
lakatostamas <mail.tamaslakatos@gmail.com>
2018-11-05 09:01:00 +0100

    tests(LinkedList): add addToTail tests

--- src/datastructures/LinkedList.test.js
+++ src/datastructures/LinkedList.test.js
@@ -22,3 +22,20 @@ test('LinkedList addToHead works on non-empty list', () => {
   expect(linkedList.head.value).toBe(6);
   expect(linkedList.head.next.value).toBe(5);
 });
+
+test('LinkedList addToTail works adds node to the tail', () => {
+  const linkedList = new LinkedList();
+
+  linkedList.addToTail(5);
+
+  expect(linkedList.tail.value).toBe(5);
+});
+
+test('LinkedList addToTail works on non-empty list', () => {
+  const linkedList = new LinkedList();
+
+  linkedList.addToTail(5);
+  linkedList.addToTail(6);
+
+  expect(linkedList.tail.value).toBe(6);
+});
