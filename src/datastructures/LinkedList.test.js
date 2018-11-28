a08e9d8b0c75fe5bc6c9ff1e222017ac145b4a62
lakatostamas <mail.tamaslakatos@gmail.com>
2018-11-28 07:46:33 +0100

    tests(LinkedList): use `addNode`

--- src/datastructures/LinkedList.test.js
+++ src/datastructures/LinkedList.test.js
@@ -5,61 +5,45 @@ test('LinkedList is empty by default', () => {
   expect(linkedList.head).toBeNull();
 });
 
-test('LinkedList addToHead adds node to the head', () => {
+test('LinkedList addNode adds node to the head', () => {
   const linkedList = new LinkedList();
 
-  linkedList.addToHead(5);
+  linkedList.addNode(5);
 
   expect(linkedList.head.value).toBe(5);
 });
 
-test('LinkedList addToHead works on non-empty list', () => {
+test('LinkedList addNode works on non-empty list', () => {
   const linkedList = new LinkedList();
 
-  linkedList.addToHead(5);
-  linkedList.addToHead(6);
+  linkedList.addNode(5);
+  linkedList.addNode(6);
 
-  expect(linkedList.head.value).toBe(6);
-  expect(linkedList.head.next.value).toBe(5);
+  expect(linkedList.head.value).toBe(5);
+  expect(linkedList.head.next.value).toBe(6);
 });
 
-test('LinkedList addToTail works adds node to the tail', () => {
-  const linkedList = new LinkedList();
-
-  linkedList.addToTail(5);
-
-  expect(linkedList.tail.value).toBe(5);
-});
-
-test('LinkedList addToTail works on non-empty list', () => {
-  const linkedList = new LinkedList();
-
-  linkedList.addToTail(5);
-  linkedList.addToTail(6);
-
-  expect(linkedList.tail.value).toBe(6);
-});
 
 test('LinkedList find returns the correct node', () => {
   const linkedList = new LinkedList();
 
-  linkedList.addToHead(7);
-  linkedList.addToHead(10);
-  linkedList.addToTail(5);
+  linkedList.addNode(7);
+  linkedList.addNode(10);
+  linkedList.addNode(5);
 
-  const actualNode = linkedList.find(10);
+  const actualNode = linkedList.find(7);
 
-  expect(actualNode.next.value).toBe(7);
+  expect(actualNode.next.value).toBe(10);
 });
 
 test('LinkedList find returns the correct node (not head)', () => {
   const linkedList = new LinkedList();
 
-  linkedList.addToHead(7);
-  linkedList.addToHead(10);
-  linkedList.addToTail(5);
+  linkedList.addNode(7);
+  linkedList.addNode(10);
+  linkedList.addNode(5);
 
-  const actualNode = linkedList.find(7);
+  const actualNode = linkedList.find(10);
 
   expect(actualNode.next.value).toBe(5);
 });
