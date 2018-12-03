cfbefee4c847496aa803efee726f28bd1bcd2bd5
lakatostamas <mail.tamaslakatos@gmail.com>
2018-12-03 08:11:48 +0100

    tests(LinkedList): add tests to `delete` method

--- src/datastructures/LinkedList.test.js
+++ src/datastructures/LinkedList.test.js
@@ -55,3 +55,43 @@ test('LinkedList find works on an empty list', () => {
 
   expect(actualNode).toBeNull();
 });
+
+test('LinkedList delete removes head', () => {
+  const linkedList = new LinkedList();
+
+  linkedList.addNode(5);
+  linkedList.addNode(6);
+
+  const deletedNode = linkedList.delete(5);
+
+  expect(deletedNode.value).toBe(5);
+});
+
+test('LinkedList delete removes not head', () => {
+  const linkedList = new LinkedList();
+
+  linkedList.addNode(5);
+  linkedList.addNode(6);
+  linkedList.addNode(7);
+
+  const deletedNode = linkedList.delete(6);
+
+  expect(deletedNode.value).toBe(6);
+  expect(linkedList.head.value).toBe(5);
+  expect(linkedList.head.next.value).toBe(7);
+});
+
+test('LinkedList delete removes tail', () => {
+  const linkedList = new LinkedList();
+
+  linkedList.addNode(5);
+  linkedList.addNode(6);
+  linkedList.addNode(7);
+
+  const deletedNode = linkedList.delete(7);
+
+  expect(deletedNode.value).toBe(7);
+  expect(linkedList.head.value).toBe(5);
+  expect(linkedList.head.next.value).toBe(6);
+  expect(linkedList.head.next.next).toBe(null);
+});
