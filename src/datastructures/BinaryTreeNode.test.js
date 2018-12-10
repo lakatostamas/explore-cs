10a6bafcf3bc9a9400f77a042cf4d8fffade767b
lakatostamas <mail.tamaslakatos@gmail.com>
2018-12-10 14:43:04 +0100

    tests(BinaryTree): add height related tests

--- src/datastructures/BinaryTreeNode.test.js
+++ src/datastructures/BinaryTreeNode.test.js
@@ -6,3 +6,24 @@ test('Binary tree is empty by default', () => {
   expect(binaryTreeNode.height).toBe(0);
 });
 
+test('Binary tree set right increases height', () => {
+  const binaryTreeNode = new BinaryTreeNode(5);
+  const rightNode = new BinaryTreeNode(6);
+  const rightNode2 = new BinaryTreeNode(7);
+
+  binaryTreeNode.setRight(rightNode);
+  rightNode.setRight(rightNode2);
+
+  expect(binaryTreeNode.height).toBe(2);
+});
+
+test('Binary tree set left increases height', () => {
+  const binaryTreeNode = new BinaryTreeNode(5);
+  const leftNode = new BinaryTreeNode(6);
+  const leftNode2 = new BinaryTreeNode(7);
+
+  binaryTreeNode.setLeft(leftNode);
+  leftNode.setLeft(leftNode2);
+
+  expect(binaryTreeNode.height).toBe(2);
+});
