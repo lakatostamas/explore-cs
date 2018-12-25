cabb903c491e5d445fcb71fc2cf07ebbe637da22
lakatostamas <mail.tamaslakatos@gmail.com>
2018-12-25 15:56:22 +0100

    feat(BinarySearchTree): implement insert method

---
+++ src/datastructures/tree/BinarySearchTreeNode.js
@@ -0,0 +1,40 @@
+import BinaryTreeNode from './BinaryTreeNode';
+
+class BinarySearchTreeNode extends BinaryTreeNode {
+  constructor(value) {
+    super(value);
+  }
+
+  insert(value) {
+    if (this.value === null) {
+      this.value = value;
+      return this;
+    }
+
+    if (this.value < value) {
+      if (this.left) {
+        return this.left.insert(value);
+      }
+
+      const newNode = new BinarySearchTreeNode(value);
+      this.setLeft(newNode);
+
+      return newNode;
+    }
+
+    if (this.value > value) {
+      if (this.right) {
+        return this.right.insert(value);
+      }
+
+      const newNode = new BinarySearchTreeNode(value);
+      this.setRight(newNode);
+
+      return newNode;
+    }
+
+    return this;
+  }
+}
+
+export default BinarySearchTreeNode;
