27b39b47a7d033225d059a4de7866d694fb3e1ee
lakatostamas <mail.tamaslakatos@gmail.com>
2018-12-28 16:00:22 +0100

    tests(BinarySearchTree): add insert tests

--- src/datastructures/tree/BinarySearchTreeNode.test.js
+++ src/datastructures/tree/BinarySearchTreeNode.test.js
@@ -15,3 +15,19 @@ test('BinarySearchListNode should create an empty binary search tree', () => {
   expect(bstNode.left).toBeNull();
   expect(bstNode.right).toBeNull();
 });
+
+test('BinarySearchListNode should insert value to itself if it is empty', () => {
+  const bstNode = new BinarySearchTreeNode(null);
+
+  bstNode.insert(10);
+
+  expect(bstNode.value).toBe(10);
+});
+
+test('BinarySearchListNode should insert unique numbers', () => {
+  const bstNode = new BinarySearchTreeNode(1);
+
+  bstNode.insert(10);
+
+  expect(bstNode.right.value).toBe(10);
+});
