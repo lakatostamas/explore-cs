e59629bbc0f49cef7e0d60de279a05ef7f138cee
lakatostamas <mail.tamaslakatos@gmail.com>
2019-01-15 10:52:16 +0100

    feat(Queue): add

---
+++ src/datastructures/queue/Queue.js
@@ -0,0 +1,40 @@
+class Queue {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.queue = [];
+  }
+
+  get front() {
+    return this.queue[0];
+  }
+
+  get rear() {
+    return this.queue[this.queue.length - 1];
+  }
+
+  isFull() {
+    return this.queue.length === this.capacity;
+  }
+
+  isEmpty() {
+    return !this.queue.length;
+  }
+
+  add(item) {
+    if (this.isFull()) {
+      throw new Error('the queue is full');
+    }
+
+    this.queue.push(item);
+  }
+
+  remove() {
+    if (this.empty()) {
+      throw new Error('the queue is empty');
+    }
+
+    this.queue.shift();
+  }
+}
+
+export default Queue;
