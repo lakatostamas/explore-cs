8a374cc8f26f527df98992823d9a8b602d1d2570
lakatostamas <mail.tamaslakatos@gmail.com>
2019-01-27 21:37:44 +0100

    feat(HashTable): add get and set method

--- src/datastructures/hashtable/HashTable.js
+++ src/datastructures/hashtable/HashTable.js
@@ -1,5 +1,37 @@
 class HashTable {
+  constructor() {
+    this.list = {};
+  }
 
+  get(x) {
+    const key = hash(x);
+
+    if (!this.list[key]) {
+      return null;
+    }
+
+    const result = this.list[key].find(pairs => pairs[0] === x);
+
+    if (!result) {
+      return null;
+    }
+
+    return result[1];
+  }
+
+  set(x, y) {
+    const key = hash(x);
+
+    if (!this.list[key]) {
+      this.list[key] = [];
+    }
+
+    this.list[key].push([x, y]);
+  }
+}
+
+function hash(x) {
+  // TODO: implement
 }
 
 export default HashTable;
