9e240e6864e4c50fe5f87f955858efb9b0801008
lakatostamas <mail.tamaslakatos@gmail.com>
2019-02-01 17:08:34 +0100

    tests(HashTable): add tests to remove method

--- src/datastructures/hashtable/HashTable.test.js
+++ src/datastructures/hashtable/HashTable.test.js
@@ -19,4 +19,44 @@ describe('HashTable', () => {
     expect(hashTable.hash('a')).toBe(7);
     expect(hashTable.hash('amb')).toBe(4);
   });
+
+  test('it should store and return the correct value', () => {
+    const hashTable = new HashTable();
+    hashTable.set('foo', 'bar');
+
+    expect(hashTable.get('foo')).toBe('bar');
+  });
+
+  test('it should store and return the correct value', () => {
+    const hashTable = new HashTable();
+
+    expect(hashTable.get('foo')).toBe(null);
+  });
+
+  test('it should overwrite the value if the key is exists', () => {
+    const hashTable = new HashTable();
+
+    hashTable.set('foo', 'bar');
+    hashTable.set('foo', 'baz');
+
+    expect(hashTable.get('foo')).toBe('baz');
+  });
+
+  test('it should remove the value from hashmap', () => {
+    const hashTable = new HashTable();
+
+    hashTable.set('foo', 'bar');
+
+    expect(hashTable.remove('foo')).toBeTruthy();
+    expect(hashTable.get('foo')).toBe(null);
+  });
+
+  test('it should return false if the value not exists', () => {
+    const hashTable = new HashTable();
+
+    hashTable.set('foo', 'bar');
+
+    expect(hashTable.remove('bar')).toBeFalsy();
+    expect(hashTable.get('foo')).toBe('bar');
+  });
 });
