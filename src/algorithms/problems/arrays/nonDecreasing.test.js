cc3db4d66cf694193620984dd43c83e2d4919f9c
lakatostamas <mail.tamaslakatos@gmail.com>
2019-02-28 10:13:02 +0100

    tests(nonDecreasing): add tests

---
+++ src/algorithms/problems/arrays/nonDecreasing.test.js
@@ -0,0 +1,19 @@
+import nonDecreasing from './nonDecreasing';
+
+describe('nonDecreasing', () => {
+  test('it should return true when [4, 2, 3] is given', () => {
+    expect(nonDecreasing([4, 2, 3])).toBeTruthy();
+  });
+  test('it should return false when [4, 2, 1] is given', () => {
+    expect(nonDecreasing([4, 2, 1])).toBeFalsy();
+  });
+  test('it should return true when [2, 2, 2] is given', () => {
+    expect(nonDecreasing([2, 2, 2])).toBeTruthy();
+  });
+  test('it should return true when [1, 2, 3] is given', () => {
+    expect(nonDecreasing([1, 2, 3])).toBeTruthy();
+  });
+  test('it should return true when [3, 2, 1] is given', () => {
+    expect(nonDecreasing([3, 2, 1])).toBeFalsy();
+  });
+});
